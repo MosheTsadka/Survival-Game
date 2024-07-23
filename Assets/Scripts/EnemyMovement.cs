d643e53c7a375c8b60e625e6e9a4f0f6506d6d4a
MosheTsadka <moshe09004501@gmail.com>
2024-07-23 17:24:45 +0300

    Enemy logic
    Movement
    attack
    Refactored
    Prefab

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,55 +3,26 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [Header("elements")] private Player _player;
-
-    [Header("Spawn Sequence Related")] 
-    [SerializeField] private SpriteRenderer enemyRenderer;
-    [SerializeField] private SpriteRenderer spawnIndicator;
-    [SerializeField] private float multiplyScale;
-    private bool _hasSpawn;
+    [Header("Elements")] private Player _player;
 
     [Header("Settings")] [SerializeField] private float moveSpeed;
-    [SerializeField] private float playerDetectionRadius;
-
-    [Header("Effects")] [SerializeField] private ParticleSystem particleEffect;
-
-    [Header("Debug")] [SerializeField] private bool showGizmos;
 
     private void Awake()
     {
         _player = FindObjectOfType<Player>();
-        particleEffect = GetComponentInChildren<ParticleSystem>();
-
-        if (_player == null)
-        {
-            Debug.LogWarning("No player found, Auto-dest");
-            Destroy(gameObject);
-        }
-    }
-
-    private void Start()
-    {
-        // Hide the Renderer component
-        enemyRenderer.enabled = false;
-
-        // Show the spawn indicator
-        spawnIndicator.enabled = true;
-
-        // Scale up & down the spawn indicator
-        Vector3 targetScale = spawnIndicator.transform.localScale * multiplyScale;
-        LeanTween.scale(spawnIndicator.gameObject, targetScale, 0.3f).setLoopPingPong(4)
-            .setOnComplete(SpawnSequenceCompleted);
-
-        // Prevent following & attacking during the spawn sequence
     }
 
     private void Update()
-    {
-        if (!_hasSpawn) return;
+    { 
+        if (_player != null)
+        {
+            FollowPlayer();
+        }
+    }
 
-        FollowPlayer();
-        TryAttack();
+    public void StorePlayer(Player player)
+    {
+        this._player = player;
     }
 
     private void FollowPlayer()
@@ -60,45 +31,5 @@ public class EnemyMovement : MonoBehaviour
         Vector2 targetPosition = (Vector2)transform.position + direction * (moveSpeed * Time.deltaTime);
 
         transform.position = targetPosition;
-    }
-
-    private void SpawnSequenceCompleted()
-    {
-        // Show the enemy after x seconds
-
-        // Hide the spawn indicator
-
-        // Hide the Renderer component
-        enemyRenderer.enabled = true;
-
-        // Show the spawn indicator
-        spawnIndicator.enabled = false;
-
-        _hasSpawn = true;
-    }
-
-    private void TryAttack()
-    {
-        float distanceFromPlayer = Vector2.Distance(transform.position, _player.transform.position);
-
-        if (distanceFromPlayer <= playerDetectionRadius)
-        {
-            DestroyObject();
-        }
-    }
-
-    private void DestroyObject()
-    {
-        particleEffect.transform.parent = null;
-        particleEffect.Play();
-        Destroy(gameObject);
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (!showGizmos) return;
-
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(transform.position, playerDetectionRadius);
     }
 }
