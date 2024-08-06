d0ab84d3f2502a8965f6c0d67602de5a751d9177
MosheTsadka <moshe09004501@gmail.com>
2024-08-06 23:53:02 +0300

    Weapon auto attack

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -1,10 +1,18 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace SurvivorGame
 {
     public class Weapon : MonoBehaviour
     {
+        enum State
+        {
+            Idle, 
+            Attack
+        }
+
+        private State _state;
+        
         [Header("Elements")] 
         [SerializeField] private Transform hitDetectionTransform;
         [SerializeField] private float hitDetectionRadius;
@@ -12,17 +20,37 @@ namespace SurvivorGame
         [Header("Settings")] 
         [SerializeField] private float range;
         [SerializeField] private LayerMask enemyMask;
+        [SerializeField] private Animator animator;
+        private List<Enemy> _damagedEnemies = new List<Enemy>();
 
         [Header("Attack")] 
         [SerializeField] private int damage;
+        [SerializeField] private float attackDelay;
+        private float _attackTimer;
 
         [SerializeField] private float aimLerp;
 
+        private void Start()
+        {
+            _state = State.Idle;
+        }
+
         private void Update()
         {
-            AutoAim();
+            switch (_state)
+            {
+                case State.Idle:
+                {
+                    AutoAim();
+                    break;
+                }
 
-            Attack();
+                case State.Attack:
+                {
+                    Attack();
+                    break;
+                }
+            }
         }
 
         private void AutoAim()
@@ -33,10 +61,48 @@ namespace SurvivorGame
 
             if (closestEnemy != null)
             {
+                ManageAttack();
                 targetUpVector = (closestEnemy.transform.position - transform.position).normalized;
             }
             
             transform.up = Vector3.Lerp(transform.up, targetUpVector, Time.deltaTime * aimLerp);
+            
+            IncrimentAttackTimer();
+        }
+
+        private void ManageAttack()
+        {
+            if (_attackTimer >= attackDelay)
+            {
+                _attackTimer = 0;
+                StartAttack();
+            }
+        }
+
+        private void IncrimentAttackTimer()
+        {
+            _attackTimer += Time.deltaTime;
+        }
+
+        [NaughtyAttributes.Button]
+        private void StartAttack()
+        {
+            animator.Play("Attack");
+            _state = State.Attack;
+
+            animator.speed = 1f / attackDelay;
+        }
+
+        private void Attacking()
+        {
+            Attack();
+        }
+
+        private void StopAttack()
+        {
+            _state = State.Idle;
+            
+            _damagedEnemies.Clear();
         }
 
         private void Attack()
@@ -45,7 +111,15 @@ namespace SurvivorGame
 
             for (int i = 0; i < enemies.Length; i++)
             {
-                enemies[i].GetComponent<Enemy>().TakeDamage(damage);
+                Enemy enemy = enemies[i].GetComponent<Enemy>();
+                
+                if (!_damagedEnemies.Contains(enemy))
+                {
+                    enemy.TakeDamage(damage);
+                    _damagedEnemies.Add(enemy);
+                }
+                
+                //enemies[i].GetComponent<Enemy>().TakeDamage(damage);
             }
         }
         
